cbd59cf69e3736a54660a6df5b471a9ac502cd0e
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-10-03 18:13:50 +0000

    Added about page and home page

--- WebApp.py
+++ WebApp.py
@@ -1,6 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
+# Home page
 @app.route("/")
-def hello():
-    return "Yo dawg."
+@app.route("/home")
+def home():
+    return "<h1>Home Page.</h1>"
+
+# About page
+@app.route("/about")
+def about():
+    return "<h1>About page</h1>"
+
+# Run Flask by running the Python file
+if __name__ == '__main__':
+    app.run(debug=True)
