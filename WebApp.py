e0d6488ba3d49ff7f6f58e9efd3cb259bac6e06d
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-10-04 11:50:53 +0000

    Render html pages instead of html within the python file

--- WebApp.py
+++ WebApp.py
@@ -1,16 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 # Home page
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page.</h1>"
+    # Return our home.html file from our 'templates' folder
+    return render_template('home.html')
 
 # About page
 @app.route("/about")
 def about():
-    return "<h1>About page</h1>"
+    return render_template('about.html')
 
 # Run Flask by running the Python file
 if __name__ == '__main__':
