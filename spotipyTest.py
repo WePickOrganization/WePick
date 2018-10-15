2dd67d929e7ba6cb77f6718a5ff99f375230abba
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-10-15 13:43:35 +0000

    Working on setting up username login locally

--- spotipyTest.py
+++ spotipyTest.py
@@ -12,7 +12,10 @@ username = input("Enter username: ")
 # https://developer.spotify.com/documentation/general/guides/scopes/
 scope = 'user-read-email'
 
-# 
+# This token is generated in the web browser
+# Can change redirect_uri to website name soon and parse it somehow
+# Once token has been generated, copy into command prompt
+# This should only have to be done once hopefully.
 token = util.prompt_for_user_token(username,scope,client_id='e6b98ce6b2cf483c832c652aada81bea',client_secret='5325fce64c6b4c4aad72b34029085111',redirect_uri='http://google.com/')
 
 
