f0a32f243b4f648fe32ffe47f413753139319a84
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-10-15 13:49:29 +0000

    Setup 4 forms for username, email and password with confirmation

--- forms.py
+++ forms.py
@@ -1,5 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
+# Setup a a function to handle registration of a user
 class RegistrationForm(FlaskForm):
-    username = StringField('Username')
+    # Username field
+    username = StringField('Username', 
+                         validators=[DataRequired(), Length(min=2, max=20)])
+    # Email Field
+    email = StringField('Email', 
+                        validators=[DataRequired(), Email()])
+    # Password field
+    password = PasswordField('Password',
+                            validators=[DataRequired])
+    # Password confirmation field
+    passwordConfirmation = PasswordField('Confirm Password',
+                            validators=[DataRequired(), EqualTo('password')])
+            
