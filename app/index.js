3355ad0bf1d7f4fe477da8e31ab1e1a33f412574
unknown <dananasas@gmail.com>
2019-01-31 14:46:02 +0000

    writing loader to transform JSX into Javascript

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-
+//The entry point is the outermost component class the React app.
 var React = require('react');
 var ReactDOM = require('react-dom');
 var App = require('./components/App');
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,21 @@
+//In Node.js __dirname refers to the currently executing file.
+//Webpack grabs the outermost component class file.
 module.exports = {
-    entry: __dirname + '/app/index.js'
+    entry: __dirname + '/app/index.js',
+    //Module will explain what webpack will do once the code has been grabbed. 
+    module: {
+        /*Each "loader" that is added to the loaders array will represent a transformation,
+          that the code will go through before reaching the browser*/
+        loaders: [
+            {   //The test property specifies which files will be affected by the loader.
+                //This regexp is for all strings that end with .js
+                test: /\.js$/,
+                //No .js files in node_modules to be transformed.
+                exclude: /node_modules/,
+                //The transformation to be executed. The babel-loader will transform all the JSX in Javascript.
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    output: {}
 };
