3f371b0ddbaeda995ca8e85604080b86bc991b5e
unknown <dananasas@gmail.com>
2019-01-31 14:00:55 +0000

    setting up entry point for app

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var App = require('./components/App');
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    entry: __dirname + '/app/index.js'
+};
