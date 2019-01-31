477a0154319bff2ef38f330ca2b0b6b681ab6a51
unknown <dananasas@gmail.com>
2019-01-31 15:51:58 +0000

    finished set up

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
-import React from "react"
-import ReactDOM from "react-dom"
+//import React from "react"
+//import ReactDOM from "react-dom"
 //The entry point is the outermost component class the React app.
 var React = require('react');
 var ReactDOM = require('react-dom');
