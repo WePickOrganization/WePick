6e5a90be765a4e660c16f1d267f409fff0b76289
Eddie Eldridge <steadyeddie101@hotmail.co.uk>
2019-02-07 12:45:10 +0000

    Added GET request method

--- flask-backend/WebApp.py
+++ flask-backend/WebApp.py
@@ -7,6 +7,7 @@ from db import DatabaseConnector
 from bson.objectid import ObjectId
 from flask import Flask
 from flask_pymongo import PyMongo
+import logger
 
 # Create instance of DatabaseConnector
 databaseConnection = DatabaseConnector.DatabaseConnector()
@@ -17,7 +18,39 @@ app = Flask(__name__, template_folder='../react-frontend/templates', static_fold
 # Prepare the mongo instance
 app.config["MONGO_URI"] = databaseConnection.getURI()
 
-print(databaseConnection.getURI())
+# Create the Mongo object with our Flask app
+mongo = PyMongo(app)
+
+# Extend the JSONEncoder class to support more stuff
+class JSONEncoder(json.JSONEncoder):
+
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, datetime.datetime):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
+# use the modified encoder class to handle ObjectId & datetime object while jsonifying the response.
+app.json_encoder = JSONEncoder
+
+
+
+# Define the routes through our Flask application
+@app.route('/user', methods=['GET', 'POST', 'DELETE', 'PATCH'])
+def user():
+    # If the HTTP Request is a 'GET' request
+    if request.method == 'GET':
+        
+        # Take the query from the HTTP request argumments
+        query = request.args
+
+        # Query the database and get the data from the query
+        data = mongo.db.WePickUsers.find_one(query)
+
+    # Return the information as JSON with status code 200
+    return jsonify(data), 200
 
 @app.route("/")
 def index():
