395ccb6c6e741a21d28492a5204d00173ccf7292
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2019-02-12 13:56:51 +0000

    Abstracted methods and routes into seperate functions

--- flask-backend/WebApp.py
+++ flask-backend/WebApp.py
@@ -31,14 +31,15 @@ class JSONEncoder(json.JSONEncoder):
         if isinstance(o, datetime.datetime):
             return str(o)
         return json.JSONEncoder.default(self, o)
+    
 
 
 # use the modified encoder class to handle ObjectId & datetime object while jsonifying the response.
 app.json_encoder = JSONEncoder
 
 # Define the routes through our Flask application
-@app.route('/user', methods=['GET', 'POST', 'DELETE', 'PATCH'])
-def user():
+@app.route('/showUser', methods=['GET'])
+def showUser():
     # If the HTTP Request is a 'GET' request
     if request.method == 'GET':
         
@@ -54,7 +55,13 @@ def user():
         # Return the information as JSON with status code 200
         return jsonify(databaseResponse), 200
 
-    data = request.get_json(force=True)
+# Route for creating a user
+@app.route('/createUser', methods=['POST'])
+def createUser():
+
+    # Define the incoming json data from the request as 'data'
+    jsonData = request.get_json(force=True)
+
     # If the HTTP Request is a 'POST' request
     if request.method == 'POST':
 
@@ -62,23 +69,30 @@ def user():
         print("\n - POST REQUEST RECIEVED - \n")
 
         # If the data is in the correct format
-        if data.get('fav_artist', None) is not None and data.get('name', None) is not None:
+        if jsonData.get('fav_artist', None) is not None and jsonData.get('name', None) is not None:
             # Successful response
-            mongo.db.Users.insert_one(data)
+            mongo.db.Users.insert_one(jsonData)
             return jsonify({'ok': True, 'message': 'User created successfully!'}), 200
         else:
             # Return a bad request response in JSON if the paramaters are incorrect
             return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
 
-    # If the HTTP Request is a 'DELETE' request
+# Route for deleting a user
+@app.route('/deleteUser', methods=['DELETE'])
+def deleteUser():
+
+    # Define the incoming json data from the request as 'data'
+    jsonData = request.get_json(force=True)
+
+     # If the HTTP Request is a 'DELETE' request
     if request.method == 'DELETE':
 
         # Show that a GET request is being recieved
         print("\n - DELETE REQUEST RECIEVED - \n")
 
         # If the data is in the correct format
-        if data.get('name', None) is not None:
-            db_response = mongo.db.Users.delete_one({'name': data['name']})
+        if jsonData.get('name', None) is not None:
+            db_response = mongo.db.Users.delete_one({'name': jsonData['name']})
             if db_response.deleted_count == 1:
                 response = {'ok': True, 'message': 'Record deleted'}
             else:
@@ -87,6 +101,13 @@ def user():
         else:
           return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
 
+
+@app.route('updateUser', methods=['PATCH'])
+def updateUser():
+
+    # Define the incoming json data from the request as 'data'
+    jsonData = request.get_json(force=True)
+
     # If the HTTP Request is a 'PATCH' request
     if request.method == 'PATCH':
 
@@ -94,18 +115,20 @@ def user():
         print("\n - PATCH REQUEST RECIEVED - \n")
 
         # If the data is in the correct format
-        if data.get('query', {}) != {}:
+        if jsonData.get('query', {}) != {}:
             mongo.db.Users.update_one(
-                data['query'], {'$set': data.get('payload', {})})
+                jsonData['query'], {'$set': jsonData.get('payload', {})})
             return jsonify({'ok': True, 'message': 'Record updated'}), 200
         else:
           return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
 
+# The default route for the app
 @app.route("/")
 def index():
   return render_template('index.html')
-
+    
 print("Starting Flask server...")
 
+# Run the app
 if __name__ == '__main__':
   app.run()
