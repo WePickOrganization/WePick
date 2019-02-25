5a8b3548a3073197c4d76f91eacce704d01da585
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2019-02-25 14:58:10 +0000

    Added basic form with no functionality and added React router

--- react-frontend/src/index.jsx
+++ react-frontend/src/index.jsx
@@ -3,6 +3,7 @@
 //The entry point is the outermost component class the React app.
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route, Link, NavLink } from 'react-router-dom';
 
 import './stylesheets/app.css'
 import './stylesheets/index.css'
@@ -33,6 +34,7 @@ class App extends React.Component {
 
       render() {
         return (
+          <Router>
           <div className= "App">
               <div className= 'App__Aside'></div>
               
@@ -41,9 +43,46 @@ class App extends React.Component {
                     <a href="#" className="PageSwitcher__Item PageSwitcher__Item">Sign In</a>
                     <a href="#" className="PageSwitcher__Item PageSwitcher__Item--Active">Sign Up</a>
                 </div>
+
+                <div className="FormTitle">
+                  <a href="#" className="FormTitle__Link">Sign In</a> or <a href="#"
+                     className="FormTitle__Link FormTitle__Link--Active">Sign Up</a>
+                </div>
+
+                <div className="FormCenter">
+                  <form className="FormFields" onSubmit={this.handleSubmit}>
+                        <div className="FormField">
+                        <label className="FormField__Label" htmlFor="name">Full Name</label>
+                        <input type="text" id="name" className="FormField__Input" placeholder="Enter your full name" name="name"></input>
+                        </div>
+                        
+                        <div className="FormField">
+                        <label className="FormField__Label" htmlFor="email">Email</label>
+                        <input type="email" id="email" className="FormField__Input" placeholder="Enter your email address" name="email"></input>
+                        </div>
+
+                        <div className="FormField">
+                        <label className="FormField__Label" htmlFor="password">Password</label>
+                        <input type="Password" id="Password" className="FormField__Input" placeholder="Enter your password" name="password"></input>
+                        </div>
+
+                        <div className="FormField">
+                        <label className="FormField__CheckboxLabel">
+                            <input className="FormField__Checkbox" type="checkbox" name="hasAgreed"/> I agree all statements in <a href="" className="FormField__TermsLink">terms of service</a>
+                        </label>
+                        </div>
+                  
+                        <div className="FormField">
+                          <button className="FormField__Button mr-20">Sign Up</button> <a href="#"
+                          className="FormField__Link">I'm already member</a>
+                        </div>
+                  </form>
+                </div>
               </div>
 
           </div>
+          </Router>
+
         )
       }
 }
