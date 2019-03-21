52aaa639d556f8ed7958577aa695569d491b21c5
unknown <dananasas@gmail.com>
2019-03-21 00:56:09 +0000

    added icons to footer

--- react-frontend/src/components/StickyFooter.js
+++ react-frontend/src/components/StickyFooter.js
@@ -9,7 +9,7 @@ class StickyFooter extends Component{
         <MDBContainer fluid className="text-center text-md-left">
           <MDBRow>
             <MDBCol md="6">
-              <h5 className="title">Footer Content</h5>
+              <h5 className="title">Find us on:</h5>
               <p>
                 Here you can use rows and columns here to organize your footer
                 content.
@@ -34,6 +34,35 @@ class StickyFooter extends Component{
             </MDBCol>
           </MDBRow>
         </MDBContainer>
+      <div className="text-center">
+        <ul className="list-unstyled list-inline">
+          <li className="list-inline-item">
+            <a className="btn-floating btn-sm btn-fb mx-1">
+              <i className="fab fa-facebook-f"> </i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a className="btn-floating btn-sm btn-tw mx-1">
+              <i className="fab fa-twitter"> </i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a className="btn-floating btn-sm btn-gplus mx-1">
+              <i className="fab fa-google-plus"> </i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a className="btn-floating btn-sm btn-li mx-1">
+              <i className="fab fa-linkedin-in"> </i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a className="btn-floating btn-sm btn-dribbble mx-1">
+              <i className="fab fa-dribbble"> </i>
+            </a>
+          </li>
+        </ul>
+      </div>
         <div className="footer-copyright text-center py-3">
           <MDBContainer fluid>
             &copy; {new Date().getFullYear()} Copyright: <a href="https://www.MDBootstrap.com"> MDBootstrap.com </a>
